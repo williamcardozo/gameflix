58bd2dd18f1b17b0b0aa93c5ec036a584999db4c
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2018-12-09 20:33:22 -0200

    refactoring no header da sidebar

--- app/src/ui/screens/logged-user-base/logged-user-base.screen.js
+++ app/src/ui/screens/logged-user-base/logged-user-base.screen.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react'
 import { GameCard, Image } from '@components'
 import './style.scss'
 
+const SidebarHeader = (props) => (
+  <header>
+    <div className="bottom-content">
+      <div className="user">
+
+        <img src="https://i.pinimg.com/originals/76/34/f0/7634f00d8f0e15f697e5cf9fb99a0d47.jpg" />
+        <span className="greetings">
+          Olá, <span className="name">William!</span>
+        </span>
+
+        <div className="quit">
+          <button onClick={() => alert('saiu')}> Sair  {Image.ICONS.Exit}</button>
+        </div>
+      </div>
+    </div>
+  </header>
+)
+
 export class LoggedUserBase extends Component {
+
 
   render() {
     return (
       <div className="base-container">
         <div className="sidebar">
-          <header>
-            
-            <div className="bottom-content">
-              <div className="user">
-                <img src="https://i.pinimg.com/originals/76/34/f0/7634f00d8f0e15f697e5cf9fb99a0d47.jpg" />
-                <span className="greetings">
-                  Olá, <span className="name">William!</span>
-                </span>
-
-                <div className="quit">
-                  <button onClick={() => alert('saiu')}> Sair  {Image.ICONS.Exit}</button>
-                </div>
-              </div>
-            </div>
-          </header>
+          <SidebarHeader {...props} />
         </div>
         <div className="screen-content">
           <GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
