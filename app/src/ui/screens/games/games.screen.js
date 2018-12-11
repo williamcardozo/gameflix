4ffd34f6ea2d12a942263ecf189c450ce9b40d6f
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2018-12-11 20:52:31 -0200

    adding game list static screen

--- app/src/ui/screens/games/games.screen.js
+++ app/src/ui/screens/games/games.screen.js
@@ -6,15 +6,33 @@ export class GamesScreen extends Component {
 
 	render () {
 		return (
-			<Fragment>
-				<h2>Games</h2>
-				<Input label="Digite o nome do seu jogo" placeholder="Buscar Jogos" type="text"/>
-				<Button className="add-game-button" typeClass="button-primary">
-					<span>{Image.ICONS.Gamepad} Adicionar Jogo</span>
-				</Button>
-				<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-				<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-				<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
+				<Fragment>
+					<h2>Games</h2>
+					<div className="games-container">
+						<header>
+							<Input label="Buscar Jogos" placeholder="Digite o nome do seu jogo" type="text"/>
+							<Button className="add-game-button" typeClass="button-primary">
+								<span>{Image.ICONS.Gamepad} Adicionar Jogo</span>
+							</Button>
+						</header>
+						<section className="games-list">
+							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
+							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
+							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
+							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
+							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
+							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
+							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
+							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
+							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
+							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
+							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
+							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
+							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
+							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
+
+						</section>
+					</div>
 			</Fragment>
 		)
 	}
