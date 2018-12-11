a8f134848796f2a9f50a1c8961892dabfda1dbca
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2018-12-11 21:15:25 -0200

    adding search method to games screen

--- app/src/ui/screens/games/games.screen.js
+++ app/src/ui/screens/games/games.screen.js
@@ -3,6 +3,42 @@ import { GameCard, Input, Button, Image } from '@components'
 import './styles.scss'
 
 export class GamesScreen extends Component {
+	state = {
+		initialGames: [],
+		games: []
+	}
+
+	componentDidMount() {
+		const games = [ 
+			{ 
+				name: 'Monster Hunter World', 
+				image: 'https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg' 
+			}, 
+			{
+				name: 'Red Dead Redemption 2', 
+				image: 'https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg' 
+			}, 
+			{
+				name: 'God of War', 
+				image: 'https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg' 
+			}  
+		]
+
+		this.setState({ games, initialGames: games })
+	}
+
+	onSearchChange = (event) => {
+		const target = event.target
+
+		const filteredGames = this.state.initialGames.filter(game => game.name.includes(target.value)) 
+		this.setState({ games: filteredGames })
+	}
+
+	renderGameCard() {
+		return this.state.games.map(game => (
+			<GameCard name={game.name} image={game.image} />
+		))
+	}
 
 	render () {
 		return (
@@ -10,27 +46,13 @@ export class GamesScreen extends Component {
 					<h2>Games</h2>
 					<div className="games-container">
 						<header>
-							<Input label="Buscar Jogos" placeholder="Digite o nome do seu jogo" type="text"/>
+							<Input label="Buscar Jogos" placeholder="Digite o nome do seu jogo" type="text" onChange={this.onSearchChange}/>
 							<Button className="add-game-button" typeClass="button-primary">
 								<span>{Image.ICONS.Gamepad} Adicionar Jogo</span>
 							</Button>
 						</header>
 						<section className="games-list">
-							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
-							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
-							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
-							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-							<GameCard name="God of Warr" image="https://images-na.ssl-images-amazon.com/images/I/51po2bu7VnL.jpg" />
-							<GameCard name="Monster Hunter World" image="https://s3.amazonaws.com/comparegame/thumbnails/43130/large.jpg" />
-							<GameCard name="Red Dead Redemption 2" image="https://images-na.ssl-images-amazon.com/images/I/91C8piUiI0L._SX342_.jpg" />
-
+							{this.renderGameCard()}
 						</section>
 					</div>
 			</Fragment>
