40640aa1e1f47e7e6c548837f2abe373971a79f4
William Goulardt Cardozo <willian-cardozo@hotmail.com>
2018-12-11 23:27:14 -0200

    adding add form section

--- app/src/ui/screens/games/games.screen.js
+++ app/src/ui/screens/games/games.screen.js
@@ -5,7 +5,10 @@ import './styles.scss'
 export class GamesScreen extends Component {
 	state = {
 		initialGames: [],
-		games: []
+		games: [],
+		gameName: '',
+		gameImageUrl: '',
+		shouldRenderForm: false
 	}
 
 	componentDidMount() {
@@ -34,6 +37,48 @@ export class GamesScreen extends Component {
 		this.setState({ games: filteredGames })
 	}
 
+	onAddGameClick = () => {
+		this.setState({ shouldRenderForm: !this.state.shouldRenderForm})
+	}
+
+	handleChange = (event) => {
+		const target = event.target
+
+		this.setState({ [target.name]: target.value })
+	}
+
+	addGame = (event) => {
+		event.preventDefault()
+	}
+
+	renderAddForm() {
+		return (
+			<div className="form-container">
+				<div className="form-container-wrapper">
+					<div>
+						<h3>Novo Jogo</h3>
+						<form className="form-game" onSubmit={this.addGame}>
+								<Input label="Nome" placeholder="Nome do jogo" 
+									type="text" onChange={this.handleChange} value={this.state.gameName}
+									name="gameName"/>
+								<Input label="Url da Imagem" placeholder="Url da Imagem do jogo" 
+									type="text" onChange={this.handleChange} value={this.state.gameImageUrl}
+									name="gameImageUrl"/>
+
+								<Button className="add-game-button" typeClass="button-primary" type="submit">
+									<span>Salvar</span>
+								</Button>
+
+						</form>
+					</div>
+					<GameCard name={this.state.gameName} image={this.state.gameImageUrl} />		
+				</div>
+			
+				<div className="line"></div>
+			</div>			
+		)
+	}
+
 	renderGameCard() {
 		return this.state.games.map(game => (
 			<GameCard name={game.name} image={game.image} />
@@ -47,10 +92,11 @@ export class GamesScreen extends Component {
 					<div className="games-container">
 						<header>
 							<Input label="Buscar Jogos" placeholder="Digite o nome do seu jogo" type="text" onChange={this.onSearchChange}/>
-							<Button className="add-game-button" typeClass="button-primary">
+							<Button className="add-game-button" typeClass="button-primary" onClick={this.onAddGameClick}>
 								<span>{Image.ICONS.Gamepad} Adicionar Jogo</span>
 							</Button>
 						</header>
+						{this.state.shouldRenderForm && this.renderAddForm()}
 						<section className="games-list">
 							{this.renderGameCard()}
 						</section>
